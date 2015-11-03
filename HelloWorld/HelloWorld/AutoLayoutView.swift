9b45fb5fc3f215077f507d004d98631148de579d
Kalson Kalu <Kalson.Kalu@yahoo.com>
2015-11-03 12:06:47 -0800

    Created a Custom Hello Button

---
+++ HelloWorld/HelloWorld/AutoLayoutView.swift
@@ -0,0 +1,13 @@
+//
+//  AutoLayoutView.swift
+//  HelloWorld
+//
+//  Created by KaL on 11/3/15.
+//  Copyright © 2015 KaL. All rights reserved.
+//
+
+import Cocoa
+
+class AutoLayoutView: UIView {
+
+}
